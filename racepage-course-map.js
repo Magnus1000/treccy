e4ff79e9bbf84b86f7f52d2f2ff7ee4cf797f7db
Magnus1000 <30488192+Magnus1000@users.noreply.github.com>
2023-09-04 14:06:00 -0400

    Update racepage-course-map.js

--- racepage-course-map.js
+++ racepage-course-map.js
@@ -27,7 +27,7 @@
   })
   .then(response => response.json())
   .then(geojsonData => {
-    console.log("Fetched GeoJSON Data:", geojsonData); // Debugging line
+    console.log("Fetched GeoJSON Data:", geojsonData);
 
     // Initialize Mapbox
     mapboxgl.accessToken = 'pk.eyJ1IjoibWFnbnVzMTk5MyIsImEiOiJjbGwyOHUxZTcyYTc1M2VwZDhzZGY3bG13In0._jM6tBke0CyM5_udTKGDOQ';
@@ -38,7 +38,44 @@
 
     // Add GeoJSON data to the map
     map.on('load', function() {
-      // All your existing code for adding the line and markers
+      map.addSource('route', {
+        type: 'geojson',
+        data: geojsonData
+      });
+
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: 'route',
+        layout: {},
+        paint: {
+          'line-color': '#888',
+          'line-width': 8
+        }
+      });
+
+      // Calculate bounds of the GeoJSON data
+      const coordinates = geojsonData.features[0].geometry.coordinates;
+      let bounds = coordinates.reduce(function(bounds, coord) {
+        return bounds.extend(coord);
+      }, new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]));
+
+      // Fit map to bounds
+      map.fitBounds(bounds, {
+        padding: 20
+      });
+
+      // Create a marker for the start point and add it to the map
+      const startPoint = coordinates[0];
+      new mapboxgl.Marker({ color: 'green' })
+        .setLngLat(startPoint)
+        .addTo(map);
+
+      // Create a marker for the end point and add it to the map
+      const endPoint = coordinates[coordinates.length - 1];
+      new mapboxgl.Marker({ color: 'red' })
+        .setLngLat(endPoint)
+        .addTo(map);
     });
   })
   .catch(error => {
