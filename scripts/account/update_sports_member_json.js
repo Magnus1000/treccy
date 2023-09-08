3f9e68e95b1ef0c8ed49d72d6ebf4eb834860aae
Magnus1000 <30488192+Magnus1000@users.noreply.github.com>
2023-09-07 21:12:19 -0400

    Update update_sports_member_json.js

--- scripts/account/update_sports_member_json.js
+++ scripts/account/update_sports_member_json.js
@@ -1,40 +1,21 @@
 <script>
-  // Function to apply the active tag to checkboxes based on sports in memberJson
-  async function setActiveCheckboxes(memberJson) {
-    console.log('Fetched memberJson:', memberJson);
-
-    memberJson.sports.forEach(sport => {
-      const checkbox = document.querySelector(`.disciplinefilter_checkbox[filter-value="${sport}"]`);
-
-      if (checkbox) {
-        checkbox.checked = true;
-        checkbox.closest('.w-checkbox').classList.add('active-filter');
-        console.log(`Added active-filter to the checkbox for ${sport}`);
-      }
-    });
-  }
-
-  // Assuming that memberstack is globally accessible
-  const memberstack = window.$memberstackDom;
-
-  async function updateMemberJsonOnFormSubmit() {
-    // Check if memberstack is defined
-    if (typeof memberstack === 'undefined') {
-      console.error('MemberStack is not defined');
-      return;
-    }
+// Define the updateMemberJsonOnFormSubmit function as globally accessible
+async function updateMemberJsonOnFormSubmit() {
+    // Here, we directly get a reference to MemberStack DOM each time we call this function
+    const memberstack = window.$memberstackDom;
 
     let member = await memberstack.getCurrentMember();
     let memberJson = await memberstack.getMemberJSON();
+
     while (memberJson.data) {
       memberJson = memberJson.data;
     }
 
     // Check if 'sports' exists in memberJson, if not initialize it
     if (!memberJson.sports) {
-      memberJson.sports = [];
+        memberJson.sports = [];
     }
-  
+
     // Initialize a new array to hold selected sports
     let newSportsArray = [];
 
@@ -47,12 +28,43 @@
       }
     });
 
+    // Replace the entire 'sports' array with the new array
     memberJson.sports = newSportsArray;
 
     await memberstack.updateMemberJSON({ json: memberJson }).then(() => {
       console.log('Member data updated:', memberJson);
     }).catch((error) => {
       console.error('Failed to update member JSON:', error);
+    });
+}
+
+// Wait for MemberStack to load
+  let checkMemberstack = setInterval(() => {
+    if (typeof window.$memberstackDom !== 'undefined') {
+      clearInterval(checkMemberstack);
+      initScript();
+    }
+  }, 100);
+
+  function initScript() {
+    const memberstack = window.$memberstackDom;
+    document.addEventListener('DOMContentLoaded', (event) => {
+      const submitButton = document.getElementById('submit-preferences');
+
+      if (submitButton) {
+        submitButton.addEventListener('click', async function(e) {
+          e.preventDefault();
+          console.log("Button clicked.");
+
+          if (typeof memberstack === 'undefined') {
+            console.error('MemberStack is not defined');
+            return;
+          }
+          await updateMemberJsonOnFormSubmit();
+        });
+      } else {
+        console.error('Button with ID submit-preferences not found');
+      }
     });
   }
 </script>
