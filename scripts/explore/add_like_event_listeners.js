421cfe60dd82fae436fba929a23afdf2ffb4a9da
Magnus1000 <30488192+Magnus1000@users.noreply.github.com>
2023-09-08 14:06:32 -0400

    Create add_like_event_listeners.js

---
+++ scripts/explore/add_like_event_listeners.js
@@ -0,0 +1,90 @@
+<script>
+// Function to initialize like buttons
+function initLikeButtons() {
+    const memberstack = window.$memberstackDom;
+    let likeButtons = document.querySelectorAll('.like-button');
+
+    // Log the count of "like-button" elements found
+    console.log(`Found ${likeButtons.length} 'like-button' elements.`);
+
+    likeButtons.forEach(likeButton => {
+        let likeButtonLastClickedAt = 0;
+
+        // Log that we're adding an event listener to this button
+        console.log('Adding event listener to a like-button.');
+
+        likeButton.addEventListener('click', async function () {
+            const now = Date.now();
+            if (now - likeButtonLastClickedAt < 500) {
+                return;
+            }
+            likeButtonLastClickedAt = now;
+
+            const algoliaObjectID = this.getAttribute('data-object-id');
+            this.classList.toggle('is-liked');  
+
+            let member = await memberstack.getCurrentMember();
+            if (!member || !member.data) {
+                window.location.href = '/sign-up-auth';
+                return;
+            }
+
+            let memberJson = await memberstack.getMemberJSON();
+            while (memberJson.data) {
+                memberJson = memberJson.data;
+            }
+
+            if (!memberJson.likes) {
+                memberJson.likes = [];
+            }
+
+            const index = memberJson.likes.findIndex(item => item.id === algoliaObjectID);
+
+            if (index === -1) {
+                memberJson.likes.push({ id: algoliaObjectID, timestamp: Date.now() });
+            } else {
+                memberJson.likes.splice(index, 1);
+            }
+
+            memberstack.updateMemberJSON({ json: memberJson }).catch((error) => {
+                console.error("Failed to update member JSON: ", error);
+                this.classList.toggle('is-liked');
+            });
+        });
+
+        (async function initButtonState() {
+            let member = await memberstack.getCurrentMember();
+            if (!member || !member.data) {
+                return;
+            }
+
+            let memberJson = await memberstack.getMemberJSON();
+            while (memberJson.data) {
+                memberJson = memberJson.data;
+            }
+
+            let algoliaObjectID = likeButton.getAttribute('data-object-id');
+            const isLiked = memberJson.likes && memberJson.likes.some(item => item.id === algoliaObjectID);
+            if (isLiked) {
+                likeButton.classList.add('is-liked');
+            }
+        })();
+    });
+
+    // Log that event listeners have been successfully added
+    console.log('Successfully added event listeners to all like-buttons.');
+}
+
+// Initialize like buttons when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded. Initializing like-buttons.');
+    initLikeButtons();
+});
+
+// Call this function whenever you load new content
+// For example, after fetching new results from Algolia
+function onNewContentAdded() {
+    console.log('New content added. Reinitializing like-buttons.');
+    initLikeButtons();
+}
+</script>
