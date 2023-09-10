be44d6d2e17e1915f4f0480de96fbb56f0d02bc7
Magnus1000 <30488192+Magnus1000@users.noreply.github.com>
2023-09-10 10:52:35 -0400

    Update fetch_and_load_algolia_region_results.js

--- scripts/region/fetch_and_load_algolia_region_results.js
+++ scripts/region/fetch_and_load_algolia_region_results.js
@@ -46,7 +46,7 @@ function populateRaceCards(results) {
         const formattedDistances = formatDistances(result.distances_ag);
 
         // Populating the card data
-        newRaceCard.querySelector('.race-card-top-block').href = `/races/${result.slug_ag}`;
+        newRaceCard.querySelector('.race-card-top-block').href = `/race/${result.slug_ag}`;
         newRaceCard.querySelector('.race-card-image').src = result.photo_main_ag;
         newRaceCard.querySelector('.race-card-image').alt = result.name_ag;
         newRaceCard.querySelector('.card-text-link-block').href = `/races/${result.slug_ag}`;
