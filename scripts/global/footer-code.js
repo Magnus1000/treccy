ae1e85e842aaf0f4ae1a9a77282c97c2b591765b
Magnus1000 <30488192+Magnus1000@users.noreply.github.com>
2023-09-14 09:21:36 -0400

    Update footer-code.js

--- scripts/global/footer-code.js
+++ scripts/global/footer-code.js
@@ -10,3 +10,29 @@ function formatDate(dateString) {
   console.log("Formatted date:", formattedDate);
   return formattedDate;
 }
+
+// Function to initialize like buttons
+function initLikeButtons() {
+  const memberstack = window.$memberstackDom;
+  let likeButtons = document.querySelectorAll('.like-button');
+  
+  console.log(`Found ${likeButtons.length} 'like-button' elements.`);
+  
+  likeButtons.forEach(likeButton => {
+    // Your existing logic here
+  });
+  
+  console.log('Successfully added event listeners to all like-buttons.');
+}
+
+// Initialize like buttons when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded. Initializing like-buttons.');
+  initLikeButtons();
+});
+
+// Call this function whenever you load new content
+function onNewContentAdded() {
+  console.log('New content added. Reinitializing like-buttons.');
+  initLikeButtons();
+}
