d99abd475dabb790d1fcca213edb4afb86d3e785
Magnus1000 <30488192+Magnus1000@users.noreply.github.com>
2023-09-14 08:45:00 -0400

    Update footer-code.js

--- scripts/global/footer-code.js
+++ scripts/global/footer-code.js
@@ -1,16 +1,10 @@
-<script>
-  $(document).ready(function() {
-    // Get the current page's URL path
-    var path = window.location.pathname;
-
-    // Remove any leading or trailing slashes from the path
-    path = path.replace(/\/$/, "");
-    path = path.substr(1);
-
-    // Find the vertical menu item that corresponds to the current page and add the "active-vertical-menu-item" class
-    $(".vertical-menu-item[data-url='" + path + "']").addClass("active-vertical-menu-item");
-
-    // Find the horizontal menu item that corresponds to the current page and add the "active-horizontal-menu-item" class
-    $(".horizontal-menu-item[data-url='" + path + "']").addClass("active-horizontal-menu-item");
-  });
-</script>
+// Function to format date
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString('en-US', { month: 'short' });
+  const year = date.getFullYear().toString().substr(-2);
+  const formattedDate = `${day} ${month} ${year}`;
+  console.log("Formatted date:", formattedDate);
+  return formattedDate;
+}
