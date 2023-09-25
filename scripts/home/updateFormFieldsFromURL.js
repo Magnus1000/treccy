73d4cda577d973bc86113129fc83225f7bb3b3b8
Jack McDonald <jackmagnusmcdonald.com>
2023-09-25 12:03:51 -0400

    Add fallback local storage if URL params are empty

--- scripts/home/updateFormFieldsFromURL.js
+++ scripts/home/updateFormFieldsFromURL.js
@@ -6,10 +6,25 @@ const setElementValue = (id, value) => {
     }
 };
 
-// Function to update form fields based on URL parameters
+// Function to update form fields based on URL parameters or localStorageUserLocation
 const updateFormFieldsFromURL = () => {
     // Fetch the current URL and its query parameters
     const urlParams = new URLSearchParams(window.location.search);
+
+    // Check if lat, lng, and location are available in the URL params
+    let lat = urlParams.get('lat');
+    let lng = urlParams.get('lng');
+    let location = urlParams.get('location');
+
+    // If lat, lng, or location are not available in the URL params, check localStorage
+    if (!lat || !lng || !location) {
+        const localStorageUserLocation = JSON.parse(localStorage.getItem('userLocation'));
+        if (localStorageUserLocation) {
+            lat = lat || localStorageUserLocation[0];
+            lng = lng || localStorageUserLocation[1];
+            location = location || localStorageUserLocation[2];
+        }
+    }
 
     // Update sport checkboxes based on 'sport' URL params
     urlParams.forEach((value, key) => {
@@ -33,6 +48,18 @@ const updateFormFieldsFromURL = () => {
         lng: value => document.getElementById('location-search-bar')?.setAttribute('data-lon', value),
     };
 
+    // Update lat, lng, and location form fields
+    if (lat) {
+        setElementValue('latitude', lat);
+    }
+    if (lng) {
+        setElementValue('longitude', lng);
+    }
+    if (location) {
+        setElementValue('location-search-bar', location);
+    }
+
+    // Update other form fields based on the remaining URL params
     for (const [key, value] of urlParams.entries()) {
         const updateField = fieldMappings[key];
         if (updateField) {
@@ -40,5 +67,5 @@ const updateFormFieldsFromURL = () => {
         }
     }
 
-    console.log("Form fields updated based on URL parameters.");
+    console.log("Form fields updated based on URL parameters or localStorage.");
 };
