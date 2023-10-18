10110029714d85c73025eeafa46d9cc56081c330
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-10-18 08:51:17 -0400

    Return user token

--- scripts/global/globalUserMembershipMemory.js
+++ scripts/global/globalUserMembershipMemory.js
@@ -3,15 +3,15 @@ function checkUserToken() {
     const existingToken = localStorage.getItem('userToken');
     if (existingToken) {
         console.log("User token already exists in local storage");
-        return;
+        return existingToken;
     }
     // Generate a new token using UUID
     const newToken = generateUserToken();
     console.log("Generated new token: ", newToken);
     localStorage.setItem('userToken', newToken);
     console.log("Token has been saved to local storage");
+    return newToken;
 }
-
 // Function to generate a user token
 function generateUserToken() {
     return uuid.v4();
