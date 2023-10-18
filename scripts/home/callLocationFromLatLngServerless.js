0d0e2f5abf516e058fca9fff8b6d34eb7daba874
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-10-18 13:31:35 -0400

    Updated the callLocationFromLatLngServerless to align with normal mapbox search dropdown.

--- scripts/home/callLocationFromLatLngServerless.js
+++ scripts/home/callLocationFromLatLngServerless.js
@@ -1,90 +1,77 @@
 // Function to handle location fetching and integration with Vercel function
 async function getLocationAndPopulateField() {
-    // Initialize UI elements
-    const searchBar = document.getElementById("location-search-bar");
-    searchBar.placeholder = "Fetching your location";
-    document.getElementById("location-arrow-icon").style.display = "none";
-    document.getElementById("loading-spinner").style.display = "flex";
-    console.log("Started fetching location...");
+// Initialize UI elements
+const searchInput = document.getElementById("location-search-bar");
+searchInput.placeholder = "Fetching your location...";
+document.getElementById("location-arrow-icon").style.display = "none";
+document.getElementById("loading-spinner").style.display = "flex";
+console.log("Started fetching location...");
 
-    // Check for geolocation support
-    if (navigator.geolocation) {
-        // Fetch the location
-        navigator.geolocation.getCurrentPosition(async function(position) {
-            // Extract latitude and longitude
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            console.log("Latitude:", latitude);
-            console.log("Longitude:", longitude);
+// Check for geolocation support
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async function(position) {
+        // Extract latitude and longitude
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 
-            // Define the URL for the Vercel serverless function
-            const vercelFunctionURL = "https://treccy-serverside-magnus1000team.vercel.app/api/treccywebsite/fetchLocationFromLatLng.js";
+        // Define the URL for the Vercel function
+        const vercelFunctionURL = "https://your-vercel-url-here";
 
-            // Set request options for the serverless function
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ latitude, longitude }),
-            };
+        // Make a POST request to the Vercel function
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ latitude, longitude })
+        };
 
-            // Make the fetch request to the Vercel function
-            const response = await fetch(vercelFunctionURL, requestOptions);
-            const data = await response.json();
-            console.log("Received data from serverless function:", data);
+        const response = await fetch(vercelFunctionURL, requestOptions);
+        const data = await response.json();
+        console.log("Received data:", data);
 
-            // Initialize city and region variables
-            let city = "";
-            let region = "";
+        // Initialize location variables
+        let city = "";
+        let region = "";
 
-            // Loop through features to identify the city and region
-            for (const feature of data.features) {
-                if (feature.place_type.includes('place')) {
-                    city = feature.text;
-                }
-                if (feature.place_type.includes('region')) {
-                    region = feature.text;
-                }
+        // Loop through features to find city and region
+        for (const feature of data.features) {
+            if (feature.place_type.includes('place')) {
+                city = feature.text;
             }
+            if (feature.place_type.includes('region')) {
+                region = feature.text;
+            }
+        }
 
-            // Concatenate city and region
-            const location = `${city}, ${region}`;
-            console.log("Location:", location);
+        // Update the search input with new data
+        searchInput.setAttribute('data-lat', latitude);
+        searchInput.setAttribute('data-lon', longitude);
+        searchInput.setAttribute('data-region', region);
+        searchInput.setAttribute('data-city', city);
 
-            // Store location data in local storage
-            const localStorageUserLocation = [latitude, longitude, location];
-            localStorage.setItem('userLocation', JSON.stringify(localStorageUserLocation));
+        // Populate the search bar with the location
+        const placeName = [city, region].join(', ');
+        searchInput.value = placeName;
 
-            // Add location data to URL parameters
-            const urlParams = new URLSearchParams(window.location.search);
-            urlParams.set('lat', latitude);
-            urlParams.set('lng', longitude);
-            urlParams.set('location', location);
-            const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
-            window.history.replaceState({}, '', newUrl);
-
-            // Populate the search bar with the location
-            searchBar.value = location;
-
-            // Reset UI elements
-            searchBar.placeholder = "Enter location";
-            document.getElementById("location-arrow-icon").style.display = "flex";
-            document.getElementById("loading-spinner").style.display = "none";
-
-        }, function(error) {
-            // Handle errors and reset UI elements
-            console.log("Error occurred:", error);
-            searchBar.placeholder = "Enter location";
-            document.getElementById("location-arrow-icon").style.display = "flex";
-            document.getElementById("loading-spinner").style.display = "none";
-        });
-    } else {
-        // Geolocation is not supported by this browser
-        console.log("Geolocation is not supported by this browser.");
-    }
+        // Reset UI elements
+        searchInput.placeholder = "Enter location";
+        document.getElementById("location-arrow-icon").style.display = "flex";
+        document.getElementById("loading-spinner").style.display = "none";
+    },
+    function(error) {
+        console.log("An error occurred:", error);
+        // Reset UI elements
+        searchInput.placeholder = "Enter location";
+        document.getElementById("location-arrow-icon").style.display = "flex";
+        document.getElementById("loading-spinner").style.display = "none";
+    });
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
 }
 
-// Listen for the DOM content to be loaded
-document.addEventListener("DOMContentLoaded", function () {
-    const locationButton = document.getElementById("location-button");
-    locationButton.addEventListener("click", getLocationAndPopulateField);
+// Add an event listener to handle button click
+document.addEventListener("DOMContentLoaded", function() {
+const locationButton = document.getElementById("location-button");
+locationButton.addEventListener("click", getLocationAndPopulateField);
 });
