d3d6e78dce357fb6d8e5e9ad4ada4e42872ffb04
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-10-25 19:13:31 -0400

    Add logs

--- scripts/country/applyClassToChildElements.js
+++ scripts/country/applyClassToChildElements.js
@@ -3,13 +3,17 @@ const hoverChildren = document.querySelectorAll('[data-hover-children="true"]');
 
 hoverChildren.forEach((element) => {
     element.addEventListener('mouseenter', () => {
+        console.log('Mouse entered element:', element);
         element.querySelectorAll('*').forEach((child) => {
+            console.log('Adding hover class to child:', child);
             child.classList.add('hover');
         });
     });
 
     element.addEventListener('mouseleave', () => {
+        console.log('Mouse left element:', element);
         element.querySelectorAll('*').forEach((child) => {
+            console.log('Removing hover class from child:', child);
             child.classList.remove('hover');
         });
     });
