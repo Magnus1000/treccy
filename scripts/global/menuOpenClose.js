d2660e478f561210d9a3f525528df5b8ec713099
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-10-25 13:03:21 -0400

    Update to add close

--- scripts/global/menuOpenClose.js
+++ scripts/global/menuOpenClose.js
@@ -6,7 +6,21 @@ const desktopProfileDropdownDiv = document.getElementById('desktop-profile-dropd
 profilePill.addEventListener('click', () => {
     if (window.innerWidth <= 768) {
         mobileMenuOverlay.style.display = 'flex';
+        console.log('Mobile menu opened');
     } else {
         desktopProfileDropdownDiv.style.display = 'flex';
+        console.log('Desktop menu opened');
+    }
+});
+
+// Add event listener to close button
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', () => {
+    if (window.innerWidth <= 768) {
+        mobileMenuOverlay.style.display = 'none';
+        console.log('Mobile menu closed');
+    } else {
+        desktopProfileDropdownDiv.style.display = 'none';
+        console.log('Desktop menu closed');
     }
 });
